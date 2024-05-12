99d06ebf7adead0c72f771c81b91d7e18198c29f
YashLoriya02 <143203752+YashLoriya02@users.noreply.github.com>
2024-05-12 20:10:51 +0530

    changes in visualisation code

--- E6_Data_Visualization/exp6.py
+++ E6_Data_Visualization/exp6.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Lineplot using Sin and Cosine
 x1 = np.linspace(0, 10, 100)
 fig = plt.figure()
 plt.plot(x1, np.sin(x1), '-')
@@ -11,10 +12,10 @@ plt.ylabel('Y-axis')
 plt.title('Sine and Cosine Functions')
 plt.legend(['Sine', 'Cosine'])
 plt.show()
-
 plt.plot([1, 2, 3, 4], [1, 4, 9, 16])
 plt.show()
 
+# Lineplot
 x = np.linspace(0, 2, 100)
 plt.plot(x, x, label='linear')
 plt.plot(x, x**2, label='quadratic')
@@ -25,10 +26,12 @@ plt.title("Simple Plot")
 plt.legend()
 plt.show()
 
+# Simpleplot
 t = np.arange(0., 5., 0.2)
 plt.plot(t, t, 'r--', t, t*2, 'bs', t, t*3, 'g^')
 plt.show()
 
+# Subplot
 fig = plt.figure()
 ax1 = fig.add_subplot(2, 2, 1)
 ax2 = fig.add_subplot(2, 2, 2)
@@ -36,39 +39,43 @@ ax3 = fig.add_subplot(2, 2, 3)
 ax4 = fig.add_subplot(2, 2, 4)
 plt.show()
 
-plt.plot([1, 3, 2, 4], 'b-')
-plt.show()
-
+# Traditional Lineplot
 x4 = range(1, 5)
 plt.plot(x4, [xi*1.5 for xi in x4],'r')
 plt.plot(x4, [xi*3 for xi in x4],'b')
 plt.plot(x4, [xi/3.0 for xi in x4],'g')
 plt.show()
 
+# Lineplot of dots
 ax = plt.axes()
 x7 = np.linspace(0, 10, 30)
 y7 = np.sin(x7)
 plt.plot(x7, y7, 'o', color = 'black')
 plt.show()
 
+# Histogram
 data1 = np.random.randn(1000)
 plt.hist(data1)
 plt.show()
 
+# Barplot
 data2 = [5. , 25. , 50. , 20.]
 plt.bar(range(len(data2)), data2)
 plt.show()
 
+# Horizontal Barplot
 data2 = [5. , 25. , 50. , 20.]
 plt.barh(range(len(data2)), data2)
 plt.show()
 
+# Errorbar
 x9 = np.arange(0, 4, 0.2)
 y9 = np.exp(-x9)
 e1 = 0.1 * np.abs(np.random.randn(len(y9)))
 plt.errorbar(x9, y9, yerr = e1, fmt = '.-')
 plt.show()
 
+# Barplot with 2 colors
 A = [15., 30., 45., 22.]
 B = [15., 25., 50., 20.]
 z2 = range(4)
@@ -76,30 +83,25 @@ plt.bar(z2, A, color = 'b')
 plt.bar(z2, B, color = 'r', bottom = A)
 plt.show()
 
+# Piechart
 plt.figure(figsize=(7,7))
 x10 = [35, 25, 20, 20]
 labels = ['Computer', 'Electronics', 'Mechanical', 'Chemical']
 plt.pie(x10, labels=labels)
 plt.show()
 
+# Boxplot
 data3 = np.random.randn(100)
 plt.boxplot(data3)
 plt.show()
 
-x12 = range(1, 6)
-y12 = [1, 4, 6, 8, 4]
-plt.fill_between(x12, y12)
-plt.show()
-
-matrix1 = np.random.rand(10, 20)
-cp = plt.contour(matrix1)
-plt.show()
-
+# Grid
 x15 = np.arange(1, 5)
 plt.plot(x15, x15*1.5, x15, x15*3.0, x15, x15/3.0)
 plt.grid(True)
 plt.show()
 
+# Plot with ticks
 u = [5, 4, 9, 7, 8, 9, 6, 5, 7, 8]
 plt.plot(u)
 plt.xticks([2, 4, 6, 8, 10])
